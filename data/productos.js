19a628bf83c17da31913527ff769812c2c1dddd9
emgr.gonzalez.r@gmail.com <emgr.gonzalez.r@gmail.com>
2026-03-24 16:24:31 -0500

    prueba arreglo

--- data/productos.js
+++
@@ -1,7 +0,0 @@
-import { Producto } from "../js/productos.js";
-
-export const listaProductos = [
-    new Producto(1, "Sartén IMUSA", 129900, "cocina", "img/sarten.jpg"),
-    new Producto(2, "Olla a presión", 199900, "cocina", "img/olla.jpg"),
-    new Producto(3, "Cafetera", 89900, "electro", "img/cafetera.jpg")
-];
